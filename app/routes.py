4b8aaf60475334f654e503df7f951b13bd5f4494
Pedro <pedromendes7182@gmail.com>
2025-08-27 01:10:35 -0300

    feat(template): add layout structure with base, blank, and reusable footer

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,13 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/email-form')
+def email_form():
+    return render_template('email-form.html')
+
+@app.route('/process', methods=['POST'])
+def process_email():
+    return "Processamento futuro..."
